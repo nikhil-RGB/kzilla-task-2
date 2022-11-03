54a0d3e2f206809bae1b1c105a529f20cb468ac6
nikhil-RGB <bleach.ichigo.anime@gmail.com>
2022-11-04 02:48:35 +0530

    Improve UI, modify colour scheme

---
+++ basic_counter/lib/constants/Colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+//normal mode Colors
+const Color tdBlack = Color(0xFF3A3A3A);
+const Color tdGrey = Color(0xFF717171);
+const Color blackMain = Color.fromARGB(255, 50, 48, 50);
+const Color items_dark = Color.fromARGB(255, 151, 146, 151);
